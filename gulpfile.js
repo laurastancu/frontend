06d5fde02d4afa104f2517a95e8a2117bbfdcb2d
iQuest <iQuest>
2016-07-12 12:10:52 +0300

    adding jade

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,13 @@
 
 require('./gulp');
 
+var gulp = require('gulp'),
+    jade = require('gulp-jade');
+
+// run this task by typing in gulp jade in CLI
+gulp.task('jade', function() {
+    return gulp.src('src/*.jade')
+        .pipe(jade()) // pip to jade plugin
+        .pipe(gulp.dest('builds/development')); // tell gulp our output folder
+});
+

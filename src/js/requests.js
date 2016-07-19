90dfbe9324e34b74486c2ade3bb32e90da6c06ef
iQuest <iQuest>
2016-07-19 11:17:57 +0300

    no message

--- src/js/requests.js
+++ src/js/requests.js
@@ -1,13 +1,13 @@
 var url = 'http://172.16.226.36:8080/';
-newsRequest = new XMLHttpRequest();
+var newsRequest = new XMLHttpRequest();
 
 function getNews() {
     newsRequest.onreadystatechange = function() {
     if (newsRequest.readyState==4&&newsRequest.status==200) {
-      document.getElementsByClass('newsDesc').innerHTML = newsRequest.responseText;
+      document.getElementsByClassName('newsDesc').innerHTML = newsRequest.responseText;
       console.log('You made it');
     } else{
-      document.getElementsByClass('newsDesc').innerHTML = "Waiting For Server Response... ";
+      document.getElementsByClassName('newsDesc').innerHTML = "Waiting For Server Response... ";
       console.log('It`s pending...');
     } 
   }

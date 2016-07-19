ac1c24e391bbca40a46f49d19d6d709bfa6e2db6
iQuest <iQuest>
2016-07-19 13:59:36 +0300

    no message

--- src/js/requests.js
+++ src/js/requests.js
@@ -1,30 +1,19 @@
 var url = 'http://172.16.226.36:8080/';
-function getNews(){
-var XMLHttp = new XMLHttpRequest();
-var startDate=(new Date).getTime();
-    XMLHttp.onreadystatechange = function() {
-    if (XMLHttp.readyState == 4 && XMLHttp.status == 200) {
-      var response= JSON.parse(XMLHttp.responseText);
-      console.log(response);
-      showNews(response);
-    } else {
-var newsRequest = new XMLHttpRequest();
+var response= {}
 
 function getNews() {
-    newsRequest.onreadystatechange = function() {
-    if (newsRequest.readyState==4&&newsRequest.status==200) {
-      document.getElementsByClassName('newsDesc').innerHTML = newsRequest.responseText;
-      console.log('You made it');
-    } else{
-      document.getElementsByClassName('newsDesc').innerHTML = "Waiting For Server Response... ";
-      console.log('It`s pending...');
-    } 
+var XMLHttp = new XMLHttpRequest();
+var startDate=(new Date).getTime();
+
+    XMLHttp.onreadystatechange = function() {
+    if (XMLHttp.readyState == 4 && XMLHttp.status == 200) {
+      //var response= JSON.parse(XMLHttp.responseText);
+      showNews(response);
+      }
   }
-    } 
-  };
-  
-  XMLHttp.open("GET", url + "webapp/news?startDate=" + startDate, true);
-  XMLHttp.send();
+
+XMLHttp.open("GET", url + "webapp/news?startDate=" + startDate, true);
+XMLHttp.send();
 }
 function showNews(news){
    

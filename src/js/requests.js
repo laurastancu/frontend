ca09e54a2fcdd88f97d7d77932e169f9e5b92200
iQuest <iQuest>
2016-07-20 16:58:46 +0300

    no message

--- src/js/requests.js
+++ src/js/requests.js
@@ -1,6 +1,10 @@
 
 $(document).ready(function () {
-  getNews();
+  if (window.location.pathname === "/html/index.html") {
+    getNews();
+  } else if (window.location.pathname === "/html/details.html") {
+    checkNewsId();  
+  }
 });
 
 function getNews() {
@@ -16,15 +20,39 @@ var startDate = (new Date).getTime();
       }
   }
 
-XMLHttp.open("GET","../js/response.json");
-XMLHttp.send();
+  XMLHttp.open("GET","../js/response.json");
+  XMLHttp.send();
 }
+
 function showNews(news){
-  
-  var source = $("#entry-template").html();
+  placeTemplate("#entry-template", ".newsContainer", {"news": news});
+}
+
+function placeTemplate (templateSelector, containerSelector, detailsObj) {
+  var source = $(templateSelector).html();
   var template = Handlebars.compile(source);
 
-  var theNews = template(news);
+  var theNews = template(detailsObj);
+  $(containerSelector).html(theNews);
+}
 
-  $(".newsContainer").html(theNews);
+function checkNewsId() {
+  var theId = getParameterByName("id");
+  if (theId === false) {
+    location.href = "/html/index.html";
+  } else {
+    console.log("retriving the news details for: ", theId);
+
+    //placeTemplate("#ewqe", ".wqeqe", news);
+  }
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return false;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
